bdf82228d2a9db540708ebb5adc89b2209165c82
jamie rolls <jamie120@hotmail.com>
2021-07-15 15:43:18 +0100

    make shoping list form populate with existing item if clicked

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -7,12 +8,28 @@ import { ShoppingListService } from '../shopping-list.service';
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
 })
-export class ShoppingListEditComponent {
+export class ShoppingListEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', { static: false }) slForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
+
   constructor(private shoppingListService: ShoppingListService) {}
-  ingredient = {
-    ingName: '',
-    amount: '',
-  };
+
+  ngOnInit() {
+    this.subscription = this.shoppingListService.startedEditing.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.shoppingListService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount,
+        });
+      }
+    );
+  }
 
   onAddItem(form: NgForm) {
     const value = form.value;
@@ -20,5 +37,9 @@ export class ShoppingListEditComponent {
     this.shoppingListService.addIngredient(newIngredient);
     console.log(newIngredient);
     form.reset();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
